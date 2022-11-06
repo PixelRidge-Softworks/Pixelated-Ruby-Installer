0557d85dab852d7307262ff25e55462c1b4856d9
VetheonGames <connorcrawford731@gmail.com>
2022-11-06 14:53:48 -0700

    Update Gitclone class

--- classes/Gitclone.rb
+++ classes/Gitclone.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby
 # frozen_string_literal: true
 
-# class ID declaration
+# class ID declaration for the gitclone class
 class Gitclone
   require 'fileutils'
   require 'git'
@@ -15,44 +15,30 @@ class Gitclone
     @destpa = destpa
   end
 
-  def mkdirs
-    puts 'Creating directories!'
-    21.times { print '-' }
-    puts '\n'
-
-    sleep(2)
-    FileUtils.mkdir_p '/etc/pixelated/ruby/bin'
-    puts 'We have created the /etc/pixelated/ruby/bin directory!'
-
-    54.times {print '-' }
-    puts '\n'
-    sleep(2)
-  end
-
   # class method to clone git repositories
   def clone
     puts 'Cloning the requested Git Repository now!'
     41.times do
       print '-'
     end
-    puts ''
+    puts $new_line
     sleep(2)
     # this bit here simply uses the Git gem to clone the repository URL stored in
     # the '@giturl' instance variable to the path stored in the '@destpa' instance variable
     return unless $softtype == 'Pixelated-Backup'
 
-    Gitclone('https://github.com/Pixelated-Studios/Pixelated-Backup.git', path: '/etc/pixelated/ruby/bin', bare: true)
+    Gitclone('https://github.com/Pixelated-Studios/Pixelated-Backup.git', path: '/usr/local/pixelated/ruby/bin', bare: true)
 
 
     # notify the user if the repository was cloned successfully
   end
 
   def gitclone_check
-    if Dir.exist?('/etc/pixelated/ruby/bin/$softtype')
-      puts "Successfully Cloned the Repo!#{$new_line}"
+    if Dir.exist?("/usr/local/pixelated/ruby/bin/#{$softtype}")
+      puts "Successfully Cloned the Repo for #{softtype}!#{$new_line}"
     else
       puts "ERROR! Repo was not cloned! Did you give us the right link?#{$new_line}"
     end
-    puts ''
+    puts $new_line
   end
 end
