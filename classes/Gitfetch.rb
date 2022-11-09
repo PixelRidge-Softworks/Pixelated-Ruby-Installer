e30fc66eac1a9b169657c3dd17e9e254b92becb5
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 03:07:32 -0700

    Create a class and method for fetching the git repo

---
+++ classes/Gitfetch.rb
@@ -0,0 +1,11 @@
+#/bin/ruby
+
+require 'rubygems'
+require 'git'
+# class for fetching an already cloned repo
+class Gitfetch
+  def initialize
+    Git.open("/etc/pixelated/ruby/bin/#{$softtype}").fetch
+    puts "Successfully fetched #{$softtype}"
+  end
+end
