e20ec3f4f1e81afdf8eaba55b749d33787793577
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 03:08:11 -0700

    Add a check and a way to handle it when the directory for a software already exists plus an update method

--- classes/Makedirectories.rb
+++ classes/Makedirectories.rb
@@ -4,15 +4,23 @@
 # class for creating directories
 class Makedirectories
   def initialize
-    puts 'Creating directories!'
-    puts '-' * 21
+    if Dir.exist?("/etc/pixelated/ruby/bin/#{$softtype}")
+      $dirsreal = 1
+      puts "We've detected that /etc/pixelated/ruby/bin/#{$softtype} already exists!"
+      puts 'Checking for Updates instead!'
+      Gitfetch.new
+    else
+      $dirsreal = 0
+      puts 'Creating directories!'
+      puts '-' * 21
 
-    sleep(2)
-    FileUtils.mkdir_p "/etc/pixelated/ruby/bin/#{$softtype}"
-    puts "We have created the /etc/pixelated/ruby/bin/#{$softtype} directory!"
+      sleep(2)
+      FileUtils.mkdir_p "/etc/pixelated/ruby/bin/#{$softtype}"
+      puts "We have created the /etc/pixelated/ruby/bin/#{$softtype} directory!"
 
-    puts '-' * 54
-    puts
-    sleep(2)
+      puts '-' * 54
+      puts
+      sleep(2)
+    end
   end
 end
