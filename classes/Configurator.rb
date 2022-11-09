f27ba9f0576e88347ae360dbf9340088ab789963
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 04:15:33 -0700

    Rough in the configurator.

--- classes/Configurator.rb
+++ classes/Configurator.rb
@@ -1,4 +1,13 @@
 #!/bin/ruby
 # frozen_string_literal: true
 
-
+# this is the class that will configure the software for us
+class Configurator
+  puts 'Launching the configurator now!'
+  if $softtype == 'Pixelated-Backup'
+    puts 'Pixelated-Backup comes pre-configured for you!'
+    puts 'No need to do anything!'
+    puts 'Exiting now!'
+    #  elsif $softtype == 'nil'
+  end
+end
