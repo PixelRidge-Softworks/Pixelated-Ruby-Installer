66d62d73992a5ac0d06ffcc451b24f1b945f65e4
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 04:17:43 -0700

    Add comments

--- classes/Passgen.rb
+++ classes/Passgen.rb
@@ -1,8 +1,10 @@
 #!/bin/ruby
 # frozen_string_literal: true
 
+# This class will generate and encrypt a password for a new user
+# This class is currently unused. It's just here for future use
 class Passgen
-### BEGIN PASSWORD GENERATOR ###
+  ### BEGIN PASSWORD GENERATOR ###
   def do_create_user(no, us, pw)
     pwd = pw.crypt('$5$a1')
     1.upto(no) do |x|
