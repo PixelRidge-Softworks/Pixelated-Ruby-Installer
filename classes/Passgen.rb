d88b9a171a23a3b196089791d36b4fb1940a2fde
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 03:26:46 -0700

    fix some things in the class

--- classes/Passgen.rb
+++ classes/Passgen.rb
@@ -1,17 +1,19 @@
 #!/bin/ruby
 # frozen_string_literal: true
 
+class Passgen
 ### BEGIN PASSWORD GENERATOR ###
-def do_create_user(no, us, pw)
-  pwd = pw.crypt('$5$a1')
-  1.upto(no) do |x|
-    uname = us + x.to_s
-    result = system("useradd -m -p '#{pwd}' #{uname}")
-    if result
-      puts "#{uname} created!"
-    else
-      puts "#{uname} failed!"
+  def do_create_user(no, us, pw)
+    pwd = pw.crypt('$5$a1')
+    1.upto(no) do |x|
+      uname = us + x.to_s
+      result = system("useradd -m -p '#{pwd}' #{uname}")
+      if result
+        puts "#{uname} created!"
+      else
+        puts "#{uname} failed!"
+      end
     end
   end
- end
+end
 ### END PASSWORD GENERATOR ###
