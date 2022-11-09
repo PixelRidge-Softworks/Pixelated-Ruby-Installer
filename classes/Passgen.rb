b0d072439f596d02ba13ee5c8dcf8c836bf076d6
VetheonGames <connorcrawford731@gmail.com>
2022-11-09 03:23:19 -0700

    Move the password Generator to it's own class

---
+++ classes/Passgen.rb
@@ -0,0 +1,17 @@
+#!/bin/ruby
+# frozen_string_literal: true
+
+### BEGIN PASSWORD GENERATOR ###
+def do_create_user(no, us, pw)
+  pwd = pw.crypt('$5$a1')
+  1.upto(no) do |x|
+    uname = us + x.to_s
+    result = system("useradd -m -p '#{pwd}' #{uname}")
+    if result
+      puts "#{uname} created!"
+    else
+      puts "#{uname} failed!"
+    end
+  end
+ end
+### END PASSWORD GENERATOR ###
