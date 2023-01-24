67c45845bfbe80bd5394db08a605d3da67a3f2b2
JClark <mlitvinkox96@gmail.com>
2023-01-24 18:17:53 -0500

    Added additional argument to initialize (softtype), added instance variable of softtype. Change Global softtype to Instance softtype variable

--- classes/Gitclone.rb
+++ classes/Gitclone.rb
@@ -8,11 +8,12 @@ class Gitclone
   attr_reader :giturl, :destpa
 
   # init class method to set instance variables for giturl and destination path
-  def initialize(giturl, destpa)
+  def initialize(giturl, destpa, softtype)
     @giturl = giturl
     @destpa = destpa
-    $giturl = giturl
-    $destpa = destpa
+    @softtype = softtype
+    # $giturl = giturl
+    # $destpa = destpa
   end
 
   # class method to clone git repositories
@@ -21,7 +22,7 @@ class Gitclone
     puts '-' * 41
     puts
     sleep(2)
-    g = Git.open("/etc/pixelated/ruby/bin/#{$softtype}")
+    g = Git.open("/etc/pixelated/ruby/bin/#{@softtype}")
     # this bit here simply uses the Git gem to clone the repository URL stored in
     # the '@giturl' instance variable to the path stored in the '@destpa' instance variable
     # return unless $softtype == 'Pixelated-Backup'
@@ -30,8 +31,8 @@ class Gitclone
 
   # notify the user if the repository was cloned successfully
   def self.check
-    if Dir.exist?("/etc/pixelated/ruby/bin/#{$softtype}/.git")
-      puts "Successfully Cloned the Repo for #{$softtype}!"
+    if Dir.exist?("/etc/pixelated/ruby/bin/#{@softtype}/.git")
+      puts "Successfully Cloned the Repo for #{@softtype}!"
       puts
     else
       puts 'ERROR! Repo was not cloned!'
